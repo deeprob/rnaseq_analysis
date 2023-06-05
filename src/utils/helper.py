0a811c28f05748ea5c0229ffd9a4613e80216db9
Deepro Banerjee <dzb5732@laila.bx.psu.edu>
2023-06-05 10:12:38 -0400

    deseq2-mingc-str-input

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -160,6 +160,6 @@ def get_formula(design_matrix_file):
 
 def run_deseq2(counts_file, counts_cols, designfile, design_formula, contrast, de_file, normcts_file, mingenecounts):
 	counts_cols_to_str = ",".join(counts_cols)
-	cmd = ["Rscript", deseq2_script_path, counts_file, counts_cols_to_str, designfile, design_formula, contrast, de_file, normcts_file, mingenecounts]
+	cmd = ["Rscript", deseq2_script_path, counts_file, counts_cols_to_str, designfile, design_formula, contrast, de_file, normcts_file, str(mingenecounts)]
 	subprocess.run(cmd)
 	return
